02a77c424ec7f532e090e84fbdd15f5667faf9ef
andrearippa <andrea.rippa@outlook.it>
2024-01-04 16:09:36 +0100

    Aggiunta di Getters/Setters per classe Utente, rimozione di final dagli attributi di classe Utente.

--- src/main/java/Model/Utente.java
+++ src/main/java/Model/Utente.java
@@ -6,10 +6,10 @@ public class Utente {
 
     //Attributi
     private String nickname;
-    private final String name;
-    private final String surname;
-    private final Date birthdate;
-    private final String gender;
+    private String name;
+    private String surname;
+    private Date birthdate;
+    private String gender;
 
     //Costruttore
     public Utente(String nickname, String name, String surname, Date birthdate, String gender){
@@ -24,9 +24,42 @@ public class Utente {
     public void setNicknameUtente(String nickname) {
         this.nickname = nickname;
     }
+
+    public void setNameUtente(String name){
+        this.name = name;
+    }
+
+    public void setSurnameUtente(String surname){
+        this.surname = surname;
+    }
+
+    public void setBirthdateUtente(Date birthdate){
+        this.birthdate = birthdate;
+    }
+
+    public void setGenderUtente(String gender){
+        this.gender = gender;
+    }
+
     //Getters
     public String getNicknameUtente(){
         return nickname;
+    }
+
+    public String getNameUtente(){
+        return name;
+    }
+
+    public String getSurnameUtente(){
+        return surname;
+    }
+
+    public Date getBirthdateUtente(){
+        return birthdate;
+    }
+
+    public String getGenderUtente(){
+        return gender;
     }
 
 }
