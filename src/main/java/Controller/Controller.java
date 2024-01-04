ee9f0dd4498a5c37277626be924399299320e9a0
andrearippa <andrea.rippa@outlook.it>
2024-01-04 20:26:04 +0100

    Modifiche a Controller

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,4 +1,13 @@
 package Controller;
+import Model.*;
+import ImpDAO.*;
+
 
 public class Controller {
+
+    Utente activeUser = new Utente();
+
+    UtenteDAOImp utenteDAO = new UtenteDAOImp();
+
+
 }
